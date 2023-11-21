8875ab31718f0f95b3eacd72fc80f6a6d8102109
Shaun Wang <spxwang@gmail.com>
2023-11-21 17:14:39 +0300

    update tests runs-on

--- tests/xcm-transfer/kusama-para.test.ts
+++ tests/xcm-transfer/kusama-para.test.ts
@@ -7,6 +7,32 @@ import { statemine } from '../../networks/statemint'
 import buildTest from './shared'
 
 const tests = [
+  // statemine -> shiden
+  {
+    from: 'statemine',
+    to: 'shiden',
+    name: 'USDT',
+    fromStorage: ({ alice }: Context) => ({
+      System: {
+        account: [[[shiden.paraAccount], { data: { free: 10e10 } }]],
+      },
+      Assets: {
+        account: [[[statemine.usdtIndex, alice.address], { balance: 1e8 }]],
+        asset: [[[statemine.usdtIndex], { supply: 1e8 }]],
+      },
+    }),
+    test: {
+      xcmPalletHorizontal: {
+        tx: tx.xcmPallet.limitedReserveTransferAssetsV3(
+          statemine.usdt,
+          1e6,
+          tx.xcmPallet.parachainV3(1, shiden.paraId),
+        ),
+        fromBalance: query.assets(statemine.usdtIndex),
+        toBalance: query.assets(shiden.usdt),
+      },
+    },
+  },
   // shiden -> statemine
   {
     from: 'shiden',
@@ -19,26 +45,9 @@ const tests = [
     }),
     test: {
       xtokenstHorizontal: {
-        tx: tx.xtokens.transfer(shiden.usdt, 1e6, tx.xtokens.parachainV3(statemine.paraId)),
+        tx: tx.xtokens.transfer(shiden.usdt, 1e7, tx.xtokens.parachainV3(statemine.paraId)),
         fromBalance: query.assets(shiden.usdt),
         toBalance: query.assets(statemine.usdtIndex),
-      },
-    },
-  },
-  // statemine -> shiden
-  {
-    from: 'statemine',
-    to: 'shiden',
-    name: 'USDT',
-    test: {
-      xcmPalletHorizontal: {
-        tx: tx.xcmPallet.limitedReserveTransferAssetsV3(
-          statemine.usdt,
-          1e6,
-          tx.xcmPallet.parachainV3(1, shiden.paraId),
-        ),
-        fromBalance: query.assets(statemine.usdtIndex),
-        toBalance: query.assets(shiden.usdt),
       },
     },
   },
