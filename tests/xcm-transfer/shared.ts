88013dd5ba81908de85628c1aa4c63664f59e8f8
Shaun Wang <spxwang@gmail.com>
2023-11-21 19:00:56 +0300

    Redact 'setTopic'.

--- tests/xcm-transfer/shared.ts
+++ tests/xcm-transfer/shared.ts
@@ -1,3 +1,5 @@
+import _ from 'lodash'
+
 import { beforeEach, describe, it } from 'vitest'
 import { sendTransaction } from '@acala-network/chopsticks-testing'
 
@@ -138,7 +140,11 @@ export default function buildTest(tests: ReadonlyArray<TestType>) {
           if ('checkUmp' in testOpt) {
             await checkUmp(fromChain).toMatchSnapshot('from chain ump messages')
           } else {
-            await checkHrmp(fromChain).toMatchSnapshot('from chain hrmp messages')
+            await checkHrmp(fromChain)
+              .map((v) => JSON.parse(JSON.stringify(v)))
+              // redact setTopic
+              .map((v) => _.update(v, '[0].data[1].v3[4].setTopic', () => 'redacted'))
+              .toMatchSnapshot('from chain hrmp messages')
           }
 
           if (routeChain) {
