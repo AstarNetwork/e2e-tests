62177b928e25939da07263e969fb867a9d3d6187
Igor Papandinas <26460174+ipapandinas@users.noreply.github.com>
2026-08-31 12:24:32 +0100

    make CI happy

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -20,6 +20,7 @@ export default tseslint.config(
 			'@typescript-eslint/require-await': 'off',
 			'@typescript-eslint/no-unsafe-member-access': 'off',
 			'@typescript-eslint/no-unsafe-argument': 'off',
+			'@typescript-eslint/no-unsafe-return': 'off',
 			'@typescript-eslint/no-unsafe-call': 'off',
 			'@typescript-eslint/no-empty-function': 'off',
 			'@typescript-eslint/explicit-module-boundary-types': 'off',
